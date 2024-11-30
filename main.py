a574e03b9923875233c0890513ae0898e523a70b
Darimir <darimrulit@gmail.com>
2024-11-30 18:17:08 +0300

    The design is converted to py files. The database and design have been moved to the UI and data folders

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import sqlite3
 
-from PyQt6 import uic
+from UI import main
+from UI import addEditCoffeeForm
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QDialog
 
@@ -9,56 +10,58 @@ from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QDialog
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi("main.ui", self)
-        self.updateButton.clicked.connect(self.updateTable)
-        self.addButton.clicked.connect(self.addCoffee)
-        self.editButton.clicked.connect(self.editCoffee)
+        self.ui = main.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.updateButton.clicked.connect(self.updateTable)
+        self.ui.addButton.clicked.connect(self.addCoffee)
+        self.ui.editButton.clicked.connect(self.editCoffee)
         self.updateTable()
 
     def updateTable(self):
-        con = sqlite3.connect("coffee.sqlite")
+        con = sqlite3.connect("data/coffee.sqlite")
         cur = con.cursor()
         result = cur.execute('SELECT * FROM Coffee').fetchall()
-        self.tableWidget.setRowCount(len(result))
+        self.ui.tableWidget.setRowCount(len(result))
         for i, row in enumerate(result):
             for j, elem in enumerate(row):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+                self.ui.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
         con.close()
 
     def addCoffee(self):
-        second_form = addEditCoffeeForm()
+        second_form = addEditCoffee()
         second_form.exec()
         self.updateTable()
 
     def editCoffee(self):
-        row = self.tableWidget.currentRow()
+        row = self.ui.tableWidget.currentRow()
         if row != -1:
-            id = self.tableWidget.item(row, 0).text()
-            second_form = addEditCoffeeForm(id)
+            id = self.ui.tableWidget.item(row, 0).text()
+            second_form = addEditCoffee(id)
             second_form.exec()
             self.updateTable()
 
 
-class addEditCoffeeForm(QDialog):
+class addEditCoffee(QDialog):
     def __init__(self, id=None):
         self.id = id
-        super(addEditCoffeeForm, self).__init__()
-        uic.loadUi("addEditCoffeeForm.ui", self)
+        super(addEditCoffee, self).__init__()
+        self.ui = addEditCoffeeForm.Ui_Form()
+        self.ui.setupUi(self)
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
+        self.ui.buttonBox.accepted.connect(self.accept)
+        self.ui.buttonBox.rejected.connect(self.reject)
 
     def accept(self):
-        con = sqlite3.connect("coffee.sqlite")
+        con = sqlite3.connect("data/coffee.sqlite")
         cur = con.cursor()
 
         data = (
-            self.variety.text(),
-            self.roastLevel.text(),
-            self.groundWhole.text(),
-            self.tasteDescription.text(),
-            self.price.value(),
-            self.packageVolume.value(),
+            self.ui.variety.text(),
+            self.ui.roastLevel.text(),
+            self.ui.groundWhole.text(),
+            self.ui.tasteDescription.text(),
+            self.ui.price.value(),
+            self.ui.packageVolume.value(),
             self.id
         )
 
